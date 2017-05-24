0d5a0e70d5475be11ef3ccb6135367ab12744934
Deanna Pham <deannahpham@gmail.com>
2017-05-23 22:12:50 -0700

    current location when open app

--- app/src/main/java/edu/ucsb/cs190i/deannahpham/deannahphamgeofencing/MapsActivity.java
+++ app/src/main/java/edu/ucsb/cs190i/deannahpham/deannahphamgeofencing/MapsActivity.java
@@ -1,19 +1,35 @@
 package edu.ucsb.cs190i.deannahpham.deannahphamgeofencing;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+// HELP FROM : https://developer.android.com/guide/topics/location/strategies.html
+//https://stackoverflow.com/questions/14478179/background-service-with-location-listener-in-android
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    final int LOCATION_PERMISSION_REQUEST_CODE = 1252;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +54,103 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Log.d("LOOKHERE", "start");
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng UCSB = new LatLng(34.4140, -119.8489);
+//        mMap.addMarker(new MarkerOptions().position(UCSB).title("Marker in UCSB"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(UCSB));
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("LOOKHERE", "permissions");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+        Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (loc == null) {
+            // fall back to network if GPS is not available
+            Log.d("LOOKHERE", "null");
+            loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        }
+        if (loc != null) {
+            Log.d("LOOKHERE", " ! null");
+            double myLat = loc.getLatitude();
+            double myLng = loc.getLongitude();
+            LatLng current = new LatLng(myLat, myLng);
+            mMap.addMarker(new MarkerOptions().position(current).title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+
+        }
+
     }
 
-//    @Override
-//    public void onMapLoaded() {
-//        //code to run when the map has loaded;
-//    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                addListenerLocation();
+            } else {
+                // if permission is not granted
+                Log.d("LOOKHERE", "no permission");
+            }
+        }
+    }
+
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+    private static double currentLat = 0;
+    private static double currentLon = 0;
+
+    private void addListenerLocation() {
+        Log.d("LOOKHERE", "in add listener location ");
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("LOOKHERE", "in location changed");
+
+                currentLat = location.getLatitude();
+                currentLon = location.getLongitude();
+                Log.d("LOOKHERE", "on location changed: current lat: " + currentLat + "current Lon: " + currentLon);
+                LatLng current = new LatLng(currentLat, currentLon);
+                mMap.addMarker(new MarkerOptions().position(current).title("Current Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("LOOKHERE", "in status changed ");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d("LOOKHERE", "in prov enabled ");
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("LOOKHERE", "in prov disabled ");
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mLocationManager.removeUpdates(mLocationListener);
+    }
 }
